f49c5a55ee8d353c020fadea948eadcaab985541
Gabe <gabe1118@yahoo.com>
2015-09-19 23:29:54 -0400

    Added Multiple Moving pictures

--- rand.js
+++ rand.js
@@ -1,42 +1,48 @@
- $(document).ready(function(){
-		animateIMG();
-		
+ $(document).ready(function(){		
+    	animateDiv($('.a'));
+    	animateDiv($('.b'));
 	});
 	 
 
-	function makeNewPosition(){
-		
-		// Get viewport dimensions (remove the dimension of the div)
-		var h = $(window).height() - 50;
-		var w = $(window).width() - 50;
-		
-		var nh = Math.floor(Math.random() * h);
-		var nw = Math.floor(Math.random() * w);
-		
-		return [nh,nw];    
-		
-	}
 
-	function animateIMG(){
-		var newq = makeNewPosition();
-		var oldq = $('img').offset();
-		var speed = calcSpeed([oldq.top, oldq.left], newq);
-		
-		$('img').animate({ top: newq[0], left: newq[1] }, speed, function(){
-		  animateIMG();        
-		});
-		
-	};
+function makeNewPosition($container) {
 
-	function calcSpeed(prev, next) {
-		
-		var x = Math.abs(prev[1] - next[1]);
-		var y = Math.abs(prev[0] - next[0]);
-		var greatest = x > y ? x : y;
-		
-		var speedModifier = 3;// control the speed here 
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $container.height() - 50;
+    var w = $container.width() - 50;
 
-		var speed = Math.ceil(greatest/speedModifier);
-		return speed;
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
 
-	}
+    return [nh, nw];
+
+}
+
+function animateDiv($target) {
+    var newq = makeNewPosition($target.parent());
+    var oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+    $target.animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function() {
+        animateDiv($target);
+    });
+
+};
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 1.1;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
